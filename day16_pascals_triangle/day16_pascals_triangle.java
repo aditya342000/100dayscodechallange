c3de621a368cb6058f10d7815e4cc386f6ea5fde
Aditya Dubey <71563664+aditya342000@users.noreply.github.com>
2022-10-30 21:51:14 +0530

    Create day16_pascals_triangle.java

---
+++ day16_pascals_triangle/day16_pascals_triangle.java
@@ -0,0 +1,21 @@
+/*
+Given an integer numRows, return the first numRows of Pascal's triangle.
+*/
+
+class Solution {
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            List<Integer> temp = new ArrayList<>();
+            int val = 1;
+            for(int j=0;j<=i;j++)
+            {
+             temp.add(val);
+             val = val*(i-j)/(j+1);
+            }
+            ans.add(temp);
+        }
+        return ans;
+    }
+}
